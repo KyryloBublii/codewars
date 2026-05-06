1db13fb5cdb175b688b00b02617367c3c8f88339
kyrylobublii <kirill.bubliy@gmail.com>
2026-05-06 19:15:23 +0300

    a new codewars task sum_two_smallest_numbers

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ def digital_root(n):
         n = sum(int(d) for d in str(n))
 
     return n
+
+"""
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+No floats or non-positive integers will be passed.
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+"""
+
+def sum_two_smallest_numbers(numbers):
+    min_values = []
+
+    for _ in range(2):
+        minimum = min(numbers)
+        min_values.append(minimum)
+        numbers.remove(minimum)
+
+    return sum(min_values)
--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 import unittest
 from main import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-    def basic_test_cases(self):
-        self.assert_equals( digital_root(16), 7, "digital_root(16)")
-        self.assert_equals( digital_root(942), 6, "digital_root(942)")
-        self.assert_equals( digital_root(132189), 6, "digital_root(132189)")
-        self.assert_equals( digital_root(493193), 2, "digital_root(493193)")
+class TestSumTwoSmallest(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]), 3453455)
+        self.assertEqual(sum_two_smallest_numbers([5, 8, 12, 18, 22]), 13)
+        self.assertEqual(sum_two_smallest_numbers([7, 15, 12, 18, 22]), 19)
+        self.assertEqual(sum_two_smallest_numbers([25, 42, 12, 18, 22]), 30)
+
+
+class TestDigitalRoot(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(digital_root(16), 7)
+        self.assertEqual(digital_root(942), 6)
+        self.assertEqual(digital_root(132189), 6)
+        self.assertEqual(digital_root(493193), 2)
+        self.assertEqual(digital_root(39512890680022105939), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
